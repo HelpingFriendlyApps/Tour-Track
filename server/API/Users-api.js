8dcf3e9638685ca3bdb9a5d693e807119ea495b7
Luis Martins <luis.m.martins91@gmail.com>
2015-11-11 18:02:49 -0500

    added some comments to model and users route

--- server/API/Users-api.js
+++ server/API/Users-api.js
@@ -4,14 +4,19 @@ var Users = require('../models/Users');
 
 var router = module.exports = express.Router();
 
+// Get all users
 router.get('/', function (req, res, next) {
 	Users.getUsers().then(function(x){ res.send(x)})
 });
 
+// Create a user
 router.post('/', function (req, res, next) {
 	Users.createUser().then(function(x){ res.send(x)})
 });
 
+// Gets a user's shows
+    // Uses the user's id to look up phish_username
+    // Takes username and call to the API asking for all shows for that user
 router.post('/phishNETaccount/:id', function (req, res, next) {
     var id = req.params.id;
 	Users.update(req.body).then(function(x){
@@ -21,6 +26,7 @@ router.post('/phishNETaccount/:id', function (req, res, next) {
 	});
 })
 
+// Gets all songs from every show
 router.get('/shows/songs/:id', function(req, res, next){
     var id = req.params.id;
     Users.getUserShows(id).then(function(userShows){
@@ -30,6 +36,7 @@ router.get('/shows/songs/:id', function(req, res, next){
     })
 });
 
+// Gets a user's shows
 router.get('/shows/:id', function(req, res, next){
     var id = req.params.id;
     Users.getUserShows(id).then(function(userShows){
@@ -37,6 +44,7 @@ router.get('/shows/:id', function(req, res, next){
     })
 });
 
+// Gets a specific user
 router.get('/:id', function (req, res, next) {
     var id = req.params.id;
     Users.getUser(id).then(function (data) {
