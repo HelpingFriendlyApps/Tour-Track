b3a12f9e996766e692fcda0699a13fb8bb23eaae
Luis Martins <luis.m.martins91@gmail.com>
2015-11-12 14:20:48 -0500

    all components now in a component folder

--- client/components/testing/testing-state.js
+++ client/components/testing/testing-state.js
@@ -2,7 +2,7 @@ angular.module('Tour-Track').config(function($stateProvider, $urlRouterProvider)
 
 	$stateProvider.state('testing', {
 		url: '/testing',
-        templateUrl: 'testing/testing.html',
+        templateUrl: 'components/testing/testing.html',
         controller: 'TestingCtrl'
 
 	});
