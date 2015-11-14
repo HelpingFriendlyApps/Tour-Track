757de9ef8614d9c76765ec62c3256fc594795e81
Luis Martins <luis.m.martins91@gmail.com>
2015-11-14 02:43:43 -0500

    fixed

--- client/components/general/general-controller.js
+++ client/components/general/general-controller.js
@@ -3,10 +3,10 @@
 angular.module('Tour-Track')
 .controller('GeneralCtrl', ['$scope','$http', 'General', function($scope, $http, General) {
 
-	// $scope.getShows = function () {
-	// 	General.shows().then(function (res) {
-	// 		$scope.allShows = res.data
-	// 	});
-	// }
+	$scope.getShows = function () {
+		General.shows().then(function (res) {
+			$scope.allShows = res.data
+		});
+	}
 
 }]);
