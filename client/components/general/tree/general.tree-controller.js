b124f3b437153fd2720ef7625d1a67429d35647f
Luis Martins <luis.m.martins91@gmail.com>
2015-11-15 02:26:21 -0500

    night night

--- client/components/general/tree/general.tree-controller.js
+++ client/components/general/tree/general.tree-controller.js
@@ -3,6 +3,14 @@
 angular.module('Tour-Track')
 .controller('GeneralLayoutTree' , ['$scope', function ($scope) {
     
-    
+    $scope.layoutTreeObject = {
+    	name: "flare",
+    	children: [
+    	{
+    		name: "analytics",
+    		size : 5000
+    	}
+    	]
+    }
 
 }]);
