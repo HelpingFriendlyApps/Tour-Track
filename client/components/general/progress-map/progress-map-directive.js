8d4c52d45c6f1220759ed690854a91031a85b1b7
Luis Martins <luis.m.martins91@gmail.com>
2015-12-17 23:40:20 -0500

    Shows now show on the map

--- client/components/general/progress-map/progress-map-directive.js
+++ client/components/general/progress-map/progress-map-directive.js
@@ -4,39 +4,59 @@ angular.module('Tour-Track').directive('progressMap', function($parse) {
         replace: true,
         template: '<div id="map"></div>',
         scope: {
-            data: '='
+            shows: '='
         },
         link: function(scope, element, attrs) {
 
             L.mapbox.accessToken = 'pk.eyJ1IjoibHVpc21hcnRpbnMiLCJhIjoiY2loZ2xsNnpwMG0xcnZia2x2Mnp3ZzYzMCJ9.huypgaYnUDo8wKLThRmyVQ';
-            var map = L.mapbox.map('map', 'mapbox.satellite')
-            	.setView([0,0],1);
+            var map = L.mapbox.map('map', 'mapbox.streets')
+                .setView([37.9, -77],4);
 
-            var earthquakesLayer = L.geoJson(null, { pointToLayer: scaledPoint })
-                .addTo(map);
 
-            function pointRadius(feature) {
-                return (feature.properties.mag - 4) * 10;
-            }
+            var rendered = false;
+            scope.$watch('shows', function(shows) {
+                if(shows && !rendered) {
+                    rendered = true;
+                    // console.log('shows', shows)
 
-            function scaledPoint(feature, latlng) {
-                return L.circleMarker(latlng, {
-                    radius: pointRadius(feature),
-                    fillColor: pointColor(feature),
-                    fillOpacity: 0.7,
-                    weight: 0.5,
-                    color: '#fff'
-                }).bindPopup(
-                '<h2>' + feature.properties.place + '</h2>' + 
-                '<h3>' + new Date(feature.properties.time) + '</h3>' +
-                feature.properties.mag + ' magnitude');
-            }
+                    var geoJsonData = {
+                        type: "FeatureCollection",
+                        features: []
+                    };
 
-            scope.$watch('data', function(shows) {
-                if(shows) {
-                    console.log('shows', shows)
+                    for(var i = 0; i < shows.length; i++) {
+                        if(shows[i].latitude && shows[i].longitude) {
+                            geoJsonData.features.push({
+                                type: 'Feature',
+                                properties: {
+                                    count: 20
+                                },
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [shows[i].longitude, shows[i].latitude]
+                                }
+                            });
+                        }
+                    }
+
+                    var radius = 500;
+
+                    var geoJson = L.geoJson(geoJsonData, {
+                        pointToLayer: function(feature, latlng) {
+                            return L.circle(latlng, radius);
+                        }
+                    }).addTo(map);
+
+
+
+
                 }
+            }, true);
+
+            map.on('zoomend', function() {
+                console.log('map.getZoom()', map.getZoom())
             })
+
 
         }
     };
