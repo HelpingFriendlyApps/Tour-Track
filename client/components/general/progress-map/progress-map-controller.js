a852b53d7d408fc064131a465d32bdf1d3855d8d
Luis Martins <luis.m.martins91@gmail.com>
2015-12-18 13:43:18 -0500

    Slider showing

--- client/components/general/progress-map/progress-map-controller.js
+++ client/components/general/progress-map/progress-map-controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
 angular.module('Tour-Track')
-.controller('ProgressMapCtrl' , ['$scope', function($scope) {
+.controller('ProgressMapCtrl', ['$scope', function($scope) {
     
+	$scope.sliderConfig = {
+		min: 50,
+		max: 500,
+		step: 1
+	}
 
+	$scope.price = 50;
+
+	$scope.setPrice = function(price) {
+		$scope.price = price;
+	}
 
 }]);
--- client/components/general/progress-map/slider-directive.js
+++ client/components/general/progress-map/slider-directive.js
@@ -1,12 +1,29 @@
-angular.module('Tour-Track').directive('progressSlider', function($parse) {
+angular.module('Tour-Track').directive('slider', function() {
     return {
-        restrict: 'E',
-        replace: true,
-        template: '<div id="slider"></div>',
+        restrict: 'A',
+        // replace: true,
+        // template: '<div id="map"></div>',
         scope: {
-            shows: '='
+            config: "=config",
+            price: "=model"
         },
         link: function(scope, element, attrs) {
+
+        	var setModel = function(value) {
+        		scope.model = value;
+        	}
+
+        	$(element).slider({
+                range: false,
+	            min: scope.config.min,
+	            max: scope.config.max,
+                step: scope.config.step,
+                slide: function(event, ui) { 
+                    scope.$apply(function() {
+                        scope.price = ui.value;
+                    });
+	            }
+	        });
 
 
 
