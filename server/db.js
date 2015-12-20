ac3e83eea55a66ec40214c4d0bd07ccf7eeb2aa4
Dan Corman <dan.corman@gmail.com>
2015-12-20 17:43:31 -0600

    Adding ternary to account for travis variable lookup

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 var config = require('../knexfile.js');  
-var env = process.env.NODE_ENV || 'development';  
+var env = process.env.NODE_ENV ? 'production' : 'development';  
 var knex = require('knex')(config[env]);
-
 module.exports = knex;
 
 knex.migrate.latest([config]); 
