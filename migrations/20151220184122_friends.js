96bd43cd1fba7bb33a0651b0c03f61759b65e994
Dan Corman <dan.corman@gmail.com>
2015-12-22 23:45:11 -0600

    hotfix

--- migrations/20151220184122_friends.js
+++ migrations/20151220184122_friends.js
@@ -14,7 +14,7 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
 
     return Promise.all([
-        knex.schema.dropTable('friends');
+        knex.schema.dropTable('friends')
     ])
 
 };
