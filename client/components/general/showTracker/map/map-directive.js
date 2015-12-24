3b49fd0f6d8e2056ac0c6b8da249a155aae4d3df
Luis Martins <luis.m.martins91@gmail.com>
2015-12-24 12:35:39 -0500

    Mapbox GL working

--- client/components/general/showTracker/map/map-directive.js
+++ client/components/general/showTracker/map/map-directive.js
@@ -11,63 +11,13 @@ angular.module('Tour-Track').directive('map', function() {
         },
         link: function(scope, element, attrs) {
 
-            L.mapbox.accessToken = 'pk.eyJ1IjoibHVpc21hcnRpbnMiLCJhIjoiY2loZ2xsNnpwMG0xcnZia2x2Mnp3ZzYzMCJ9.huypgaYnUDo8wKLThRmyVQ';
-            var map = L.mapbox.map('map', 'examples.map-h68a1pf7')
-                .setView([37.9, -77],4);
-
-            var showsLayer = L.geoJson(null, { pointToLayer: scaledPoint })
-                .addTo(map);
-
-            var radius = 50;
-            function scaledPoint(feature, latlng) {
-                return L.circle(latlng, radius);
-            }
-
-            scope.$watch('currentShow', function(currentShow) {
-                if(currentShow) {
-                    map.setView([currentShow.latitude, currentShow.longitude], 8);
-                }
-            }, true);
-
-
-            scope.$watch('shows', function(shows) {
-                if(shows) {
-                    var geoJsonData = {
-                        type: "FeatureCollection",
-                        features: []
-                    };
-
-                    for(var i = 0; i < shows.length; i++) {
-                        if(shows[i].latitude && shows[i].longitude) {
-                            geoJsonData.features.push({
-                                type: 'Feature',
-                                properties: {
-                                    count: 20
-                                },
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: [shows[i].longitude, shows[i].latitude]
-                                }
-                            });
-                        }
-                    }
-
-                    scope.$watch('progress', function(progress) {
-                        var progShows = Object.assign({}, geoJsonData)
-                        progShows.features = progShows.features.slice(0, progress);
-
-                        showsLayer.clearLayers()
-                            .addData(progShows);
-
-                    }, true);
-
-
-                }
-            }, true);
-
-            // map.on('zoomend', function() {
-            //     console.log('map.getZoom()', map.getZoom())
-            // })
+            mapboxgl.accessToken = 'pk.eyJ1IjoibHVpc21hcnRpbnMiLCJhIjoiY2loZ2xsNnpwMG0xcnZia2x2Mnp3ZzYzMCJ9.huypgaYnUDo8wKLThRmyVQ';
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/luismartins/ciijdjlxr003hzwma0fhnlxfm', //hosted style id
+                center: [-77.38, 39], // starting position
+                zoom: 3 // starting zoom
+            });
 
         }
     };
