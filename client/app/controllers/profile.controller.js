586d07036f8eaccfd0ecee4eccaf6377cad4f25e
Dan Corman <dan.corman@gmail.com>
2016-03-24 00:39:10 -0500

    Added throbber, implemented in profile page

--- client/app/controllers/profile.controller.js
+++ client/app/controllers/profile.controller.js
@@ -4,15 +4,19 @@ angular.module('Tour-Track')
 .controller('ProfileCtrl', ['$scope','Profile','$rootScope', function($scope, Profile, $rootScope) {
 
   Profile.userObject().then(function(user) {
+    $scope.showsLoading = true;
+    $scope.songsLoading = true;
     $rootScope.user = user.data;
     console.log('$scope.user', $scope.user)
     Profile.userShows($scope.user.uid).then(function(shows) {
+        $scope.showsLoading = false;
       $scope.userShows = shows;
       console.log('$scope.userShows', $scope.userShows)
       console.log('$scope.userShows.length', $scope.userShows.length)
     });
     Profile.userSongs($scope.user.uid).then(function(songs) {
-      $scope.userSongs = songs;
+    $scope.songsLoading = false;
+    $scope.userSongs = songs;
       console.log('$scope.userSongs', $scope.userSongs)
     });
   });
