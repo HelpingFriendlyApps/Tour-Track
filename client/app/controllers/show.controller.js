cebc871a62b316a6c58655fb9aceacd0ff241059
Luis Martins <luis.m.martins91@gmail.com>
2016-04-12 00:39:02 -0400

    Added set markers

--- client/app/controllers/show.controller.js
+++ client/app/controllers/show.controller.js
@@ -6,15 +6,26 @@ app.controller('ShowCtrl', function($scope, show, setlist, ShowFactory) {
   $scope.show.setlist = setlist;
   console.log('$scope.show', $scope.show)
 
+  var sets = [];
+  $scope.show.setlist.forEach(function(song) {
+    if(sets.indexOf(song.set) < 0) {
+      song.firstOfSet = true;
+      sets.push(song.set);
+      console.log('sets', sets)
+    }
+  });
+
+  $scope.setParser = function(set) {
+    if(parseInt(set)) return 'Set ' + set;
+    return 'Encore';
+  }
+
   $scope.timeParser = function(ms) {
     var min = Math.floor(ms / 60000);
     var sec = Math.floor(ms / 1000 % 60);
     if(sec < 10) sec = '0' + sec;
     return min + ':' + sec;
   }
-
-
-
 
   ShowFactory.getNextShowByDate($scope.show.date).then(function(nextShow) {
     $scope.nextShow = nextShow;
@@ -23,6 +34,5 @@ app.controller('ShowCtrl', function($scope, show, setlist, ShowFactory) {
   ShowFactory.getPrevShowByDate($scope.show.date).then(function(prevShow) {
     $scope.prevShow = prevShow;
   });
-
 
 });
