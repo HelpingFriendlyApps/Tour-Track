961d941a5e10a2f2c3b063fe3279088a969ee79a
Luis Martins <luis.m.martins91@gmail.com>
2016-04-11 23:03:56 -0400

    Styling

--- client/app/directives/map.directive.js
+++ client/app/directives/map.directive.js
@@ -4,17 +4,74 @@ app.directive('map', function(mapboxToken) {
   return {
     restrict: 'E',
     template: '<div id="map"></div>',
+    scope: {
+      coordinates: '='
+    },
     link: function(scope, element, attrs) {
 
-      console.log('scope.show', scope.show)
+      // console.log('scope.coordinates', scope.coordinates)
 
-      mapboxgl.accessToken = mapboxToken;
-      var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/luismartins/cijroc9jb006t90lx8ehn9k2v',
-        center: [scope.show.longitude, scope.show.latitude],
-        zoom: 15
-      });
+      // scope.coordinates = scope.coordinates || [-73, 44];
+
+      scope.$watch('coordinates', function(coordinates) {
+        console.log('coordinates', coordinates)
+        if(!coordinates[0]) {
+          console.log('INVAL')
+          coordinates = [-73, 44];
+        }
+
+        mapboxgl.accessToken = mapboxToken;
+        var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/luismartins/cijroc9jb006t90lx8ehn9k2v',
+          center: coordinates,
+          zoom: 15
+        });
+
+        map.on('style.load', function() {
+
+          var geoJson = {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [{
+                type: 'Feature',
+                properties: {
+                  // whatever i want
+                },
+                geometry: {
+                  type: 'Point',
+                  coordinates: coordinates
+                }
+              }]
+            }
+          };
+
+          map.addSource('show', geoJson);
+
+          map.addLayer({
+            id: 'something',
+            // interactive: true,
+            interactive: false,
+            type: 'circle',
+            source: 'show',
+            paint: {
+              'circle-radius': 30,
+              'circle-color': 'black'
+            }
+          });
+
+        });
+
+
+
+        
+      })
+
+
+
+
+
 
 
     }
