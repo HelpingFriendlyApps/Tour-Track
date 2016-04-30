157977972cf2d24f6ffa483f5988e7f597d049b3
Luis Martins <luis.m.martins91@gmail.com>
2016-04-29 22:27:27 -0400

    Refactoring / Styling

--- client/app/controllers/root.controller.js
+++ client/app/controllers/root.controller.js
@@ -2,7 +2,8 @@
 
 angular.module('Tour-Track')
 
-.controller('RootController', ['$scope','CredsFactory','$rootScope', function($scope, CredsFactory, $rootScope) {
+.controller('RootController', ['$scope','CredsFactory','$rootScope', '$state', function($scope, CredsFactory, $rootScope, $state) {
+    
     $scope.mapboxToken = "";
     $rootScope.$watch('player', (newVal, oldVal) => {
       if(!oldVal) $scope.player = $rootScope.player;
@@ -11,4 +12,5 @@ angular.module('Tour-Track')
     CredsFactory.getMapBoxToken().then( token => {
       $scope.mapboxToken = token;
     });
+
 }]);
