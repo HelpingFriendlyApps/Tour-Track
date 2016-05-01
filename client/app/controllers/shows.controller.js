93358b12bdd82cf0bf25dbd048f8120d8557bd07
Luis Martins <luis.m.martins91@gmail.com>
2016-05-01 18:03:06 -0400

    Building out show filter logic

--- client/app/controllers/shows.controller.js
+++ client/app/controllers/shows.controller.js
@@ -5,10 +5,28 @@ angular.module('Tour-Track')
 
   var years = [];
 
-  var Shows = function() {
+  console.log('allShows', allShows)
+
+  $scope.filter = { venue: "", location: "" };
+
+  $scope.$watch('filter', function(filter) {
+    console.log('filter', filter)
+    years = [];
+
+    var filteredShows = allShows.filter(function(show) {
+      return show.location.toLowerCase().indexOf(filter.location.toLowerCase()) > -1 && show.venue_name.toLowerCase().indexOf(filter.venue.toLowerCase()) > -1;
+    });
+
+    $scope.shows = new Shows(filteredShows);
+    console.log('$scope.shows', $scope.shows)
+  }, true);
+
+
+  var Shows = function(showCollection) {
     this.loadedPages = {};
     this.numItems = 0;
-    this.pageSize = 50;
+    this.pageSize = (showCollection.length < 50) ? showCollection.length : 50;
+    this.showCollection = showCollection;
     this.fetchNumItems();
   };
 
@@ -26,7 +44,7 @@ angular.module('Tour-Track')
 
   Shows.prototype.fetchPage = function(pageNumber) {
     var self = this.loadedPages[pageNumber] = [];
-    var showChunk = allShows.slice(pageNumber * this.pageSize, ++pageNumber * this.pageSize);
+    var showChunk = this.showCollection.slice(pageNumber * this.pageSize, ++pageNumber * this.pageSize);
 
     showChunk.forEach(function(show) {
       var year = show.date.slice(0,4);
@@ -40,10 +58,8 @@ angular.module('Tour-Track')
 
   Shows.prototype.fetchNumItems = function() {
     $timeout(angular.noop, 300).then(angular.bind(this, function() {
-      this.numItems = 50000;
+      this.numItems = this.showCollection.length;
     }));
   };
-
-  $scope.shows = new Shows();
 
 }]);
