2792c22a5cf01b9fa2a7fd9b91bc04ee80867b92
Luis Martins <luis.m.martins91@gmail.com>
2016-05-04 01:09:05 -0400

    Styling

--- client/app/directives/graphs/songDebutsPerYear.directive.js
+++ client/app/directives/graphs/songDebutsPerYear.directive.js
@@ -51,54 +51,17 @@ app.directive('songDebutsPerYear', ["$q", "SongFactory", function($q, SongFactor
               width: {
                 ratio: 0.5
               }
+            },
+            axis: {
+              y: {
+                show: false
+              }
             }
           });
 
-
         });
 
-
       });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      // scope.$watch('setlist', (setlist) => {
-      //   var debutsPerYear = {};
-      //   for (var year = 1983; year <= new Date().getFullYear(); year++) {
-      //     debutsPerYear[year] = 0;
-      //   }
-
-      //   setlist.forEach( (song, i) => {
-      //     SongFactory.getSongDebut(song.song_id).then(function(debutShow) {
-      //       debutsPerYear[debutShow.date.slice(0,4)]++;
-      //       if(i === setlist.length - 1) renderGraph(debutsPerYear);
-      //     });
-      //   });
-      // });
-
-
-      // function renderGraph(debutsPerYear) {
-      //   console.log('debutsPerYear top', debutsPerYear)
-
-      //   debutsPerYear = Object.keys(debutsPerYear).sort();
-      //   console.log('debutsPerYear bottom', debutsPerYear)
-
-
-
-
-      // }
 
 
     }
