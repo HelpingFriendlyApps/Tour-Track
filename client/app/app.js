742d1bba0ad211b646cd3d4e5aa422f3e9160b3e
Luis Martins <luis.m.martins91@gmail.com>
2016-09-11 17:41:03 -0400

    Abstracted states to own file

--- client/app/app.js
+++ client/app/app.js
@@ -11,45 +11,4 @@ var app = angular.module('Tour-Track', [
   'infinite-scroll',
   'angularMoment',
   'ui.bootstrap'
-])
-  
-  .config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function($stateProvider, $urlRouterProvider, $locationProvider) {
-
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider.state('shows', {
-      url: '/shows',
-      templateUrl: '../views/shows.html',
-      controller: 'ShowsCtrl',
-      resolve: {
-        allShows: function(ShowFactory) {
-          return ShowFactory.getAllShows();
-        }
-      }
-    }).state('show', {
-      url: '/show/:date',
-      templateUrl: '../views/show.html',
-      controller: 'ShowCtrl',
-      resolve: {
-        show: function(ShowFactory, $stateParams) {
-          return ShowFactory.getShowByDate($stateParams.date);
-        },
-
-        setlist: function(ShowFactory, $stateParams) {
-          return ShowFactory.getSetlistByDate($stateParams.date);
-        }
-      }
-    }).state('songs', {
-      url: '/songs',
-      templateUrl: '../views/songs.html',
-      controller: 'SongsCtrl',
-      resolve: {
-        allSongs: function(SongFactory) {
-          return SongFactory.getAllSongs();
-        }
-      }
-    });
-   
-   }
-
- ]);
+]);
---
+++ client/app/states.js
@@ -0,0 +1,39 @@
+'use strict';
+
+app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('shows', {
+    url: '/shows',
+    templateUrl: '../views/shows.html',
+    controller: 'ShowsCtrl',
+    resolve: {
+      allShows: function(ShowFactory) {
+        return ShowFactory.getAllShows();
+      }
+    }
+  }).state('show', {
+    url: '/show/:date',
+    templateUrl: '../views/show.html',
+    controller: 'ShowCtrl',
+    resolve: {
+      show: function(ShowFactory, $stateParams) {
+        return ShowFactory.getShowByDate($stateParams.date);
+      },
+      setlist: function(ShowFactory, $stateParams) {
+        return ShowFactory.getSetlistByDate($stateParams.date);
+      }
+    }
+  }).state('songs', {
+    url: '/songs',
+    templateUrl: '../views/songs.html',
+    controller: 'SongsCtrl',
+    resolve: {
+      allSongs: function(SongFactory) {
+        return SongFactory.getAllSongs();
+      }
+    }
+  });
+   
+}]);
